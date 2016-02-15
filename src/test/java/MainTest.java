766f6b9b55f60e3da05b997ea0d772af857f0953
Vladislav Gerasimov <wlad03131@gmail.com>
2016-02-15 18:07:08 +0300

    JUnit and Main class added

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void mainAvailability() {
+        Main main = new Main();
+        assert true;
+    }
+}
